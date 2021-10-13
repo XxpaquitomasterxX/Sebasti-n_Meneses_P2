d6dd299eeb4e9500262e7693083f53ac0d6d3602
Xxpaquitomaster <89699500+XxpaquitomasterxX@users.noreply.github.com>
2021-10-13 16:29:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num_1=0;
+int num_2=0;
+int num_3;
+int num_4;
+int result_1=0;
+int result_2=0;
+
+cout <<"escribir número:   ";
+cin >> num_1;//se define el valor de una variable
+cout <<"escribir número:   ";
+cin >> num_2;//se define el valor de una variable
+result_1 =num_1+num_2 ;
+cout <<"resultado de la suma: ";
+cout << result_1<<"\n";//se enseña el resultado
+cout <<"escribir número:   ";
+cin >> num_3;//se define el valor de una variable
+cout <<"escribir número:   ";
+cin >> num_4;//se define el valor de una variable
+cout<< "resultado de la resta:";
+result_2= num_3-num_4  ;
+cout <<result_2 <<"\n";//se enseña otro resultado
+cout<<"Esta sería la multiplicación de ambos resultados:" ; cout<<result_1*result_2;//se realiza una multiplicación y se enseña
+
+
+
+
+}
